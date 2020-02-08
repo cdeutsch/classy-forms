63762c8c44e7b863e0d1e97590d197936b17bfb8
Christopher Deutsch <cd@cdeutsch.com>
2020-02-07 18:47:15 -0600

    Added a simple Demo page.

---
+++ demo/components/FormInput.tsx
@@ -0,0 +1,98 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
+import { red } from '@material-ui/core/colors';
+import { FormFieldWithHelpers } from 'classy-forms';
+import React from 'react';
+
+const rootCss = css`
+  display: flex;
+  flex-direction: column;
+
+  margin-bottom: 0.75em;
+
+  label {
+    font-size: 15px;
+    font-weight: bold;
+    line-height: 2;
+  }
+
+  abbr {
+    cursor: help;
+  }
+
+  input {
+    border: 1px solid #cccccc;
+    border-radius: 3px;
+    color: #555555;
+    font-size: 13px;
+    padding: 4px;
+    width: 155px;
+
+    &:focus {
+      border-color: #111;
+      outline: 0;
+    }
+  }
+
+  p {
+    font-size: 0.75rem;
+    font-weight: 400;
+    line-height: 1em;
+    margin: 0.25em 0 0 0;
+    min-height: 1em;
+  }
+`;
+
+const errorCss = css`
+  input {
+    border-color: ${red.A400};
+
+    &:focus {
+      border-color: ${red.A700};
+    }
+  }
+
+  p {
+    color: ${red.A400};
+  }
+`;
+
+export interface FormInputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'name' | 'required' | 'value' | 'onBlur' | 'onChange'> {
+  formField: FormFieldWithHelpers;
+}
+
+export class FormInput extends React.Component<FormInputProps> {
+  render() {
+    const { formField, ...passthrough } = this.props;
+    const { hasError, helperText, label, name, onBlur, onChange, required, value } = formField;
+
+    const actualRootCss = [rootCss];
+    if (hasError) {
+      actualRootCss.push(errorCss);
+    }
+
+    return (
+      <div css={actualRootCss}>
+        <label htmlFor={name}>
+          {required ? <abbr title="required">*</abbr> : null}
+          {label}
+        </label>
+        {/* tslint:disable-next-line: react-a11y-input-elements */}
+        <input
+          autoCapitalize="off"
+          autoCorrect="off"
+          size={50}
+          type="text"
+          id={name}
+          name={name}
+          value={value}
+          onBlur={onBlur}
+          onChange={onChange}
+          {...passthrough}
+        />
+        {helperText ? <p>{helperText}</p> : null}
+      </div>
+    );
+  }
+}
--- demo/pages/index.tsx
+++ demo/pages/index.tsx
@@ -1,15 +1,57 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import { Form } from 'classy-forms';
+import { Button } from '@material-ui/core';
+import { ClassyForms, Form, FormField, FormFieldConfig, FormsContextContext } from 'classy-forms';
 import React from 'react';
 
+import { FormInput } from '../components/FormInput';
 import { Page } from '../components/Page';
+
+interface UserFormFields {
+  firstName: string;
+  lastName: string;
+}
+
+function getRequiredHelperText(formField: FormField, submitting?: boolean) {
+  return formField.hasError && !formField.value ? 'Required' : '';
+}
+
+const formFieldConfigs: FormFieldConfig[] = [
+  {
+    name: 'lastName',
+    label: 'Last name',
+    required: true,
+    getHelperText: getRequiredHelperText,
+  },
+  {
+    name: 'firstName',
+    label: 'First name',
+    required: true,
+    getHelperText: getRequiredHelperText,
+  },
+];
 
 export class Home extends React.Component {
   render() {
     return (
       <Page>
-        <Form>Hello</Form>
+        <ClassyForms formFieldConfigs={formFieldConfigs}>
+          {({ formFields: { firstName, lastName }, onSubmit }: FormsContextContext<UserFormFields>) => {
+            return (
+              <Form acceptCharset="UTF-8" action="/" id="voteForm" method="post" noValidate>
+                <FormInput key={firstName.name} formField={firstName} />
+
+                <FormInput key={lastName.name} formField={lastName} />
+
+                <div className="form-actions">
+                  <Button variant="contained" color="primary" type="submit">
+                    Register
+                  </Button>
+                </div>
+              </Form>
+            );
+          }}
+        </ClassyForms>
       </Page>
     );
   }
